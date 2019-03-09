801818af32c5c1c0aa7c9bfa3f02e9c9acd861f5
chadirahme <eng.chadi@gmail.com>
2019-03-08 23:19:34 -0500

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {DocksComponent} from "./docks/docks.component";
+import {DrainageComponent} from "./drainage/drainage.component";
+import {BrandsComponent} from "./brands/brands.component";
+import {GroheComponent} from "./grohe/grohe.component";
+import {PedrolloComponent} from "./pedrollo/pedrollo.component";
+import {CotaliComponent} from "./cotali/cotali.component";
+import {AqgComponent} from "./aqg/aqg.component";
+import {ContactComponent} from "./contact/contact.component";
+import {CategoryComponent} from "./category/category.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'drainage', component: DrainageComponent },
+  { path: 'docks', component: DocksComponent },
+  { path: 'brands', component: BrandsComponent },
+  { path: 'grohe/:name', component: GroheComponent },
+  { path: 'pedrollo', component: PedrolloComponent },
+  { path: 'cotali', component: CotaliComponent },
+  { path: 'aqg', component: AqgComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: '*', component: HomeComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DocksComponent } from './docks/docks.component';
+import { DrainageComponent } from './drainage/drainage.component';
+import { BrandsComponent } from './brands/brands.component';
+import { GroheComponent } from './grohe/grohe.component';
+import { PedrolloComponent } from './pedrollo/pedrollo.component';
+import { CotaliComponent } from './cotali/cotali.component';
+import { AqgComponent } from './aqg/aqg.component';
+import { ContactComponent } from './contact/contact.component';
+import {HttpClientModule} from "@angular/common/http";
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    DocksComponent,
+    DrainageComponent,
+    BrandsComponent,
+    GroheComponent,
+    PedrolloComponent,
+    CotaliComponent,
+    AqgComponent,
+    ContactComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,    // <-- Right here
+
   ],
   providers: [],
   bootstrap: [AppComponent]
